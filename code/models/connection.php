9e23dce5f496fd2c76580e4a2a18807ff1ce406e
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2022-12-04 22:02:05 +0100

    update database connection

--- code/models/connection.php
+++ code/models/connection.php
@@ -10,7 +10,8 @@ class Connection
 
       private function pdoConnection()
       {
-            $con = new PDO('mysql:host=' . $this->serv . ';dbname=' . $this->bd, $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $con = new PDO('mysql:host=' . $this->serv . ';dbname=' . $this->bd, $this->user, $this->pass);
+            $con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             try {
                   return $con;
             } catch (PDOException $e) {
