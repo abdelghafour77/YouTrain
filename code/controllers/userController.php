4ca10055a0fb12532d97e1438794ef14b774e220
faissal17 <faissalaoukacha@gmail.com>
2022-12-06 09:17:26 +0100

    hashing password

--- code/controllers/userController.php
+++ code/controllers/userController.php
@@ -3,6 +3,7 @@ require_once '../models/user.php';
 
 if (isset($_POST['signup'])) {
     extract($_POST);
+    $password = password_hash($password, PASSWORD_DEFAULT);
     $user = new User($email, $password, $first_name, $last_name);
     $user->registration();
 }
