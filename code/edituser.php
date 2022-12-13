5b705c951c274f856ebe067541ae3d558eb5f3d7
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2022-12-13 11:13:11 +0100

    fix style

--- code/edituser.php
+++ code/edituser.php
@@ -27,7 +27,7 @@ session_start()
 <body class="bol">
     <form action="controllers/userController.php" method="POST">
         <div class="container col-4 mb-3 mt-3">
-            <div class="card">
+            <div class="card-edit">
                 <img src="assets/img/user.png" class="img-fluid">
                 <div class="row">
                     <h1 class="text-center">Edit User Profil</h1>
