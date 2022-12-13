61897fe2a99e9ff9afe34b2e421c502cacb50ed6
Boutaina <boutaina.elatbaoui@gmail.com>
2022-12-13 18:26:46 +0100

    Added last version of login page

--- code/login.php
+++ code/login.php
@@ -16,11 +16,16 @@
     </style>
     <div class="form-style mx-auto py-4">
         <form action="./controllers/userController.php" method="POST" id="form" data-parsley-validate>
-            <h3>please login</h3>
+            <img src="assets/img/logo.webp" class="img-fluid me-2 mb-3" alt="Bootstrap" width="180" height="40">
+            <h2 class="text-white mb-3">Please Login</h2>
             <input type="email" name="email" placeholder="email" required data-parsley-trigger="keyup">
             <input type="password" name="password" placeholder="password" required data-parsley-trigger="keyup">
-            <input type="submit" name="login" value="login" class="form-btn">
-            <p class="fs-5 text-light">don't you have an account?<a class="text-decoration-none fs-5 text-danger" href="signup.php">regist now</a></p>
+            <!-- <input type="submit" name="login" value="login" class="form-btn"> -->
+            <div class="mt-3 mb-4">
+                <button type="submit" name="login" class="form-btn text-black">Login</button>
+            </div>
+            <p class="fs-5 text-light">Don't you have an account? <button class="btn btn-outline-danger btn-sm ms-1">
+                                        <a href="signup.php" class="text-decoration-none text-white">Register here</a></button></p>
         </form>
     </div>
 </body>
