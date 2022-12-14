13ad41247db721fdf463b059a1b19e15e5728003
Boutaina <boutaina.elatbaoui@gmail.com>
2022-12-14 17:02:39 +0100

    Added some changes

--- code/models/admin.php
+++ code/models/admin.php
@@ -6,5 +6,12 @@ require_once 'connection.php';
 
 class Admin extends Connection
 {
-    
+    public function allAdmins()
+    {
+        $sql = "SELECT * FROM `users` WHERE admin = 1";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }
 }
--- code/views/allAdmin.php
+++ code/views/allAdmin.php
@@ -1,3 +1,14 @@
-<div class="card-body">
-    <p class="card-text">Hello</p>
+<?php
+    include_once './models/admin.php';
+    $all_admins = new Admin();
+    $admins = $all_admins->allAdmins();
+?>
+<div class="card-body bg-light">
+    <?php
+        foreach ($admins as $admin) {
+    ?>
+    <p class="card-text fw-bold"><i class="bi bi-person-fill-check me-3 fs-5"></i> <?= $admin['first_name'] . " " . $admin['last_name']  ?></p>
+    <?php
+        }   
+    ?>
 </div>
