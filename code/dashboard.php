d5d1f06b4d9e715b8631d937eeefb606c8af02d8
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2022-12-17 20:53:35 +0100

    dynamic sidebar

--- code/dashboard.php
+++ code/dashboard.php
@@ -13,6 +13,7 @@ include("include/head.php");
     <!-- END navbar -->
     <!-- BEGIN Offcanvas -->
     <?php
+    $page = 'dashboard';
     include("include/sidebar.php");
     ?>
     <!-- END Offcanvas -->
--- code/traveldetails.php
+++ code/traveldetails.php
@@ -13,6 +13,7 @@ include("include/head.php");
     <!-- END navbar -->
     <!-- BEGIN Offcanvas -->
     <?php
+    $page = 'travels';
     include("include/sidebar.php");
     ?>
     <!-- END Offcanvas -->
